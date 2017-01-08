8f70f3449335f4e95c65b3d5db7195291ce49ada
John Van Horn <jmvanhorn@gmail.com>
2017-01-08 00:27:52 -0600

    12.1 complete

--- 12/index.js
+++ 12/index.js
@@ -1,10 +1,8 @@
 'use strict';
+/* eslint-env node */
 
 const R = require('ramda');
 const {Map, fromJS} = require('immutable');
-const input = require('fs').readFileSync('./input.txt', 'utf8')
-    .split('\n')
-    .map(R.trim);
 
 // cpy 19 c
 // jnz c -2
@@ -23,6 +21,7 @@ const parseIns = ins => {
 };
 
 const initState = ins => fromJS({
+    i: 0,
     registers: [0, 0, 0, 0],
     nextIns: 0,
     ins
@@ -36,52 +35,98 @@ const stateHalted = state => {
 
 const insHead = ins => ins.get(0);
 const cpyIns = R.pipe(insHead, R.equals('cpy'));
+const cpyValueIns = R.allPass([cpyIns, ins => R.is(Number, ins.get(1))]);
+const cpyReadIns = R.allPass([cpyIns, ins => R.is(String, ins.get(1))]);
 const decIns = R.pipe(insHead, R.equals('dec'));
 const incIns = R.pipe(insHead, R.equals('inc'));
 const jnzIns = R.pipe(insHead, R.equals('jnz'));
+const notJnzIns = R.complement(jnzIns);
+const jnzRead = R.pipe(ins => ins.get(1), R.is(String));
 const registerIndex = Map([...'abcd'].map((name, i) => [name, i]));
+const registerRead = (slot, registers) => registers.get(registerIndex.get(slot));
 
 // ins => ['inc', 'a'] | ['cpy', 1|'a', 'b']
 const execIns = (ins, registers) => {
-    console.log('execIns()', ins);
     const reg = R.cond([
         [incIns, ins => registers.update(registerIndex.get(ins.get(1)), R.inc)],
         [decIns, ins => registers.update(registerIndex.get(ins.get(1)), R.dec)],
+        [cpyValueIns, ins => registers.set(registerIndex.get(ins.get(2)), ins.get(1))],
+        [cpyReadIns, ins => registers.set(
+            registerIndex.get(ins.get(2)),
+            registerRead(ins.get(1), registers)
+        )],
+        [jnzIns, R.always(registers)],
         [R.T, () => {throw new Error(`instruction not recognized ${ins[0]}`);}]
     ])(ins);
 
-    return {
-        registers: reg
-    };
+    const insMove = R.cond([
+        [notJnzIns,  R.always(1)],
+        [jnzIns, ins => {
+            const d = jnzRead(ins) ? registerRead(ins.get(1), registers) : ins.get(1);
+            return d === 0 ? 1 : ins.get(2);
+        }]
+    ])(ins);
+
+    return fromJS({
+        registers: reg,
+        insMove
+    });
 };
 
 const execState = state => {
-    console.log('execState()', state);
-    const ins = state.get('ins').get(state.get('nextIns'));
-    const exec = execIns(ins, state.get('registers'));
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write([
+        state.get('registers').join(),
+        state.get('nextIns'),
+        state.get('i')
+    ].join(' '));
 
-    console.log('ins', ins);
-    console.log('execd', exec);
+    const ins = state.get('ins').get(state.get('nextIns'));
+    const result = execIns(ins, state.get('registers'));
 
     return state
-        .set('registers', exec.registers)
-        .update('nextIns', R.inc);
+        .set('registers', result.get('registers'))
+        .update('i', R.inc)
+        .update('nextIns', R.add(result.get('insMove')));
 };
 
 const assembunny = function*(state = initState([])){
-    //console.log('init state', state);
     let nextState = state;
-    while(!stateHalted(nextState)){
-        yield nextState = execState(state);
+
+    do {
+        yield nextState = execState(nextState);
     }
+    while(!stateHalted(nextState));
+
+    process.stdout.write('\n');
+    return nextState;
+};
+
+const runIns = ins => {
+    let state = assembunny(initState(ins));
+    let s;
+
+    do {
+        s = state.next();
+    } while(!s.done);
+
+    return s.value;
 };
 
 Object.assign(exports, {
     parseIns,
     initState,
-    assembunny
+    execIns,
+    assembunny,
+    runIns
 });
 
 if(require.main === module){
-    console.dir(input);
+    const input = require('fs').readFileSync('./input.txt', 'utf8')
+        .split('\n')
+        .map(R.trim);
+
+    const run = runIns(input.map(parseIns));
+    console.dir(R.pick(['registers', 'nextIns', 'i'], run.toJS()), {colors: true});
 }
--- 12/index.test.js
+++ 12/index.test.js
@@ -1,8 +1,12 @@
 'use strict';
 /* eslint-env mocha */
 
-const {expect} = require('chai');
-const {parseIns, assembunny, initState} = require('./');
+const chai = require('chai');
+chai.use(require('chai-immutable'));
+
+const expect = chai.expect;
+const {fromJS, List} = require('immutable');
+const {parseIns, assembunny, initState, execIns, runIns} = require('./');
 
 //[0, 0, 0, 0];
 
@@ -15,16 +19,101 @@ describe('parseIns', () => {
     });
 });
 
+describe('execIns', () => {
+    // inc a
+    // dec b
+    // cpy 19 c
+    // cpy a b   (read from a)
+
+    it('executes inc', () => {
+        const registers = fromJS([1, 1, 1, 1]);
+        const r = execIns(fromJS(['inc', 'b']), registers);
+        expect(r).to.have.property('registers', List([1, 2, 1, 1]));
+        expect(r).to.have.property('insMove', 1);
+    });
+
+    it('executes dec', () => {
+        const registers = fromJS([1, 1, 1, 1]);
+        const r = execIns(fromJS(['dec', 'c']), registers);
+        expect(r).to.have.property('registers', List([1, 1, 0, 1]));
+        expect(r).to.have.property('insMove', 1);
+    });
+
+    it('executes cpy value', () => {
+        const registers = fromJS([1, 1, 1, 1]);
+        const r = execIns(fromJS(['cpy', 5, 'd']), registers);
+        expect(r).to.have.property('registers', List([1, 1, 1, 5]));
+        expect(r).to.have.property('insMove', 1);
+    });
+
+    it('executes cpy read', () => {
+        const registers = fromJS([1, 2, 3, 4]);
+        const r = execIns(fromJS(['cpy', 'c', 'a']), registers);
+        expect(r).to.have.property('registers', List([3, 2, 3, 4]));
+        expect(r).to.have.property('insMove', 1);
+    });
+
+    // jnz 1 -2
+    // jnz 0 4   (zero noop)
+    // jnz c 3   (read from c)
+    // jnz c 3   (read from c, c == 0, noop)
+
+    it('executes jnz value > 0', () => {
+        const registers = fromJS([1, 2, 3, 4]);
+        const r = execIns(fromJS(['jnz', 1, -2]), registers);
+        expect(r).to.have.property('registers', registers);
+        expect(r).to.have.property('insMove', -2);
+    });
+
+    it('executes jnz value < 0 (noop)', () => {
+        const registers = fromJS([1, 2, 3, 4]);
+        const r = execIns(fromJS(['jnz', 0, 5]), registers);
+        expect(r).to.have.property('registers', registers);
+        expect(r).to.have.property('insMove', 1);
+    });
+
+    it('executes jnz read', () => {
+        const registers = fromJS([1, 2, 3, 4]);
+        const r = execIns(fromJS(['jnz', 'c', 9]), registers);
+        expect(r).to.have.property('registers', registers);
+        expect(r).to.have.property('insMove', 9);
+    });
+
+    it('executes jnz read reg < 0 (noop)', () => {
+        const registers = fromJS([1, 1, 1, 0]);
+        const r = execIns(fromJS(['jnz', 'd', 8]), registers);
+        expect(r).to.have.property('registers', registers);
+        expect(r).to.have.property('insMove', 1);
+    });
+});
+
 describe('assembunnyState', () => {
-    it('executes state', () => {
+    it('executes single state', () => {
         const state = assembunny(initState([
             ['inc', 'a']
         ]));
         let s = state.next();
 
-        console.log('@@', s);
+        //console.log('@@', s);
 
         expect(s.value.toJS()).to.have.deep.property('registers[0]', 1);
         expect(state.next()).to.have.property('done', true);
+    });
+
+});
+
+describe('runIns', () => {
+    it('executes program', () => {
+        const r = runIns([
+            ['cpy', 41, 'a'],
+            ['inc', 'a'],
+            ['inc', 'a'],
+            ['dec', 'a'],
+            ['jnz', 'a', 2],
+            ['dec', 'a']
+        ]);
+
+        console.log('run...', r);
+        expect(r.getIn(['registers', '0'], 42));
     });
 });
